1180c1783f11d8dbab12f978ef880b5172c6f930
Denim Mazuki <denimmazuki@gmail.com>
2018-04-16 19:51:32 -0700

    Added blackbox test for loading image gif

--- src/test/csci310P2-blackbox-tests/test.sh
+++ src/test/csci310P2-blackbox-tests/test.sh
@@ -4,4 +4,5 @@ cucumber features/register.feature
 cucumber features/login.feature
 cucumber features/collageHistory.feature
 cucumber features/collageGeneration.feature
+cucumber features/loadingImage.feature
 cucumber features/exportFunctionality.feature
